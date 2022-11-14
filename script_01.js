72807943df52f7875577cdaa42263b8bd8f5eba7
Mia Kamenetski <mia.kamenetski@online.de>
2022-11-14 11:48:52 +0100

    module: console output

--- script_01.js
+++ script_01.js
@@ -3,18 +3,42 @@
 /*
 0. a+b / a-b/ a*b / a/b  // ergebnis c 
 1. Dateneingabe + -überprüfung :
-2. Auswahl Rechenart : 
-3. Fkt. Grundrechenarten :
+2. Auswahl Rechenart : check!!
+3. Fkt. Grundrechenarten : check!!
 4. Ausgabe in Konsole : check!!
 */
 
+//module: calculator | tests:
+//Agreement : "+", "-", "*", ":"
+// output(calculator(2,2,"+"));
+// output(calculator(2,2,"-"));
+// output(calculator(2,2,"*"));
+// output(calculator(2,2,":"));
+// output(calculator(2,0,"/"));
+// output(calculator(2,2,"#"));
+
+function calculator(a,b,op) {
+    switch (op) {
+        case "+": // addition 
+            return add(a,b);
+        case "-": // subtraction
+            return subtract(a,b);
+        case "*": // multiplication
+            return multiply(a,b);  
+        case "/": // division
+        case ":": // division
+            return divide(a,b);
+        default: // Error...
+            return "Sorry - something went wrong... Try again?";
+    }
+}
+
 
 //module: division a / b | test:
-output(divide(2,3));
-output(divide(2,-3));
-output(divide(2,0));
-output(divide(0,2));
-
+// output(divide(2,3));
+// output(divide(2,-3));
+// output(divide(2,0));
+// output(divide(0,2));
 function divide(a,b) {
 
     // if (b != 0) {
@@ -27,18 +51,16 @@ function divide(a,b) {
 
     if (b == 0) {
         // false
-            return "ERROR: Division by 0 not possible!"
+            return "Division by 0 not possible!"
         }
         // true
         return a / b;
     }
 
-
 //module: multiply a * b | test:
 // output(multiply(2,3));
 // output(multiply(2,-3));
 // output(multiply(2,0));
-
 function multiply(a,b) {
     return a * b;
 }
@@ -47,7 +69,6 @@ function multiply(a,b) {
 // output(subtract(2,3));
 // output(subtract(6,3));
 // output(subtract(2,2));
-
 function subtract(a,b) {
     return a - b;
 }
@@ -56,15 +77,20 @@ function subtract(a,b) {
 // output(add(2,3));
 // output(add(2,-3));
 // output(add(2,0));
-
 function add(a,b) {
     return a + b;
 }
 
 
 //module: console output | test
-// output("hello");
-// output(2);
-function output(outputdata) {
-    console.log(outputdata);
+output("hello");
+output(2);
+function output(outputData) {
+    // console.log(typeof outputData)
+    if (typeof outputData == "number") {
+        console.log("The result is: " + outputData);
+    } else {
+        console.log("ERROR!!! " + outputData)
+    }
+
 }
